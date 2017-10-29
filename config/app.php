22fd8eb4acacb1d278bc7a3b0e01378a006a935f
xto <xot@doc-inside.com>
2017-10-29 11:56:51 +0000

    Agragado paquete de markdown y su configuracion

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
 
         //
         Styde\Html\HtmlServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Access' => Styde\Html\Facades\Access::class,
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
 
     ],
 
